5317dc571ca7570fe7fd0901fe5013c8751b852a
mlmisha <146455565+mlmisha@users.noreply.github.com>
2024-07-28 15:55:40 +0300

    Update UTR_orth.py

--- scripts_data/UTR_orth.py
+++ scripts_data/UTR_orth.py
@@ -10,7 +10,7 @@ def extractor (transctipt_id,k):
   server = "https://rest.ensembl.org"
   cds = f"/sequence/id/{transctipt_id}?type=cds"
 
-  orf = requests.get(server+cds, headers={ "Content-Type" : "text/x-fasta",'User-Agent': f'fwefweffwwfapwjefnllw{k}'})
+  orf = requests.get(server+cds, headers={ "Content-Type" : "text/x-fasta",'User-Agent': f'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15_12{k}'})
 
   if not orf.ok:
     orf.raise_for_status()
@@ -18,20 +18,17 @@ def extractor (transctipt_id,k):
   return orf.text.strip()
 
 
-with open(f"utrs_file_here", mode = "r") as data:
+with open(f"PATH_TO_FILE_WITH_3'UTRs", mode = "r") as data:
     name = data.readline().strip()
     utr_seq = data.readline().strip()
     while name != "" and utr_seq != "":
-        if name in utr_dict:
-            utr_dict[name].append(utr_seq)
-        else:
-            utr_dict[name] = [utr_seq]
+        utr_dict[name] = utr_seq
         name = data.readline().strip()
         utr_seq = data.readline().strip()
 
 
 transcripts = []
-with open (f"file_with_unique_transcript_ids_here", mode = "r") as data:
+with open (f"PATH_TO_FILE_WITH_TRANSCRIPTS_OF_ORTHOLOGOUS_GENES.tsv", mode = "r") as data:
    tran = data.readline().strip()
    while tran != "":
       transcripts.append(tran)
@@ -39,38 +36,46 @@ with open (f"file_with_unique_transcript_ids_here", mode = "r") as data:
 
 count = 0
 result = []
-stops = ["TAA", "TAG", "TGA"]
+stops = {"TAA":"", "TAG":"", "TGA":""}
+lost = []
+lost_seqs = []
 for i in transcripts:
-    print(".",end = "",flush=True)
-    if count%1000==0:
-        print()
-        print(count,xid,flush=True)
-    failed = False
-    try:
-        cds = extractor(i,count)
-    except requests.exceptions.HTTPError:
-        failed = True
-    except requests.exceptions.ConnectionError:
-        attempts = 0
-        while attempts<5:
-            try:
-              attempts+=1
-              cds = extractor(i,count)
-              break
-            except:
-              print("-",end="",flush=True)
-              time.sleep(5)
-        if attempts>=5:
+    if i in utr_dict:
+      print(".",end = "",flush=True)
+      if count%1000==0:
+          print()
+          print(count,xid,flush=True)
+      failed = False
+      try:
+          cds = extractor(i,count)
+      except requests.exceptions.HTTPError:
           failed = True
-    count+=1
-    if not failed:
-       stop_codon = cds[-3::]
-       if stop_codon in stops:
-        if i in utr_dict:
-            for utr in utr_dict[i]:
-                result.append([i,stop_codon, len(utr)])
+      except requests.exceptions.ConnectionError:
+          attempts = 0
+          while attempts<5:
+              try:
+                attempts+=1
+                cds = extractor(i,count)
+                break
+              except:
+                print("-",end="",flush=True)
+                time.sleep(5)
+          if attempts>=5:
+            failed = True
+      count+=1
+      if not failed:
+        stop_codon = cds[-3::]
+        if stop_codon in stops:
+          result.append([i, stop_codon,len(utr_dict[i])])
+      else:
+         lost.append(i)
+         lost_seqs.append(utr_dict[i])
+with open(f"../data/ORTHS/ORTHRES_{xid}.tsv", mode = "w") as out:
+   print ("")
+   for i in result:
+      print(*i, sep="\t", file = out)
 
-with open(f"../data/RES_Human_or_Rabbit.tsv", mode = "w") as out:
+with open(f"PATH_to_lost_dir/lost_{xid}.tsv", mode = "w") as out: #to manage 3'UTRs lost due to some request errors
    print ("")
    for i in result:
       print(*i, sep="\t", file = out)
